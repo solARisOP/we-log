8c3270b65706457956c1587bb0f40dfe7fa590d8
solARis <nitishraosnr2001@gmail.com>
2023-09-26 20:27:34 +0530

    26/09/23 - made user authetication system and added users in it, and made template and model for comments

--- home/views.py
+++ home/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
 from home.models import Contact
 from django.contrib import messages
 from blog.models import Post
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
 
 def home(request):
     return render(request, "home/home.html")
@@ -37,4 +39,54 @@ def search(request):
         messages.warning(request, "No search results found please refine your query")
     params = {'allPosts' : allPosts, 'query' : query}
     return render(request, "home/search.html", params)
-    # return HttpResponse("this is search")
+
+def handleSignup(request):
+    if request.method == 'POST':
+        username= request.POST['username']
+        fname= request.POST['fname']
+        lname= request.POST['lname']
+        email= request.POST['email']
+        pass1= request.POST['pass1']
+        pass2= request.POST['pass2']
+        if not username.isalnum():
+            messages.error(request, "username must contain only aphabets and numbers")
+            return redirect('home')
+        if pass1 != pass2:
+            messages.error(request, "Passwords do not match")
+            return redirect('home')
+
+        myuser = User.objects.create_user(username, email, pass1)
+        myuser.first_name = fname
+        myuser.last_name = lname
+        myuser.save()
+        messages.success(request, "Your weLog account has been created successfully")
+        return redirect('home')
+
+    else:
+        return HttpResponse("Request not found", status=400)
+    
+def handleLogin(request):
+    if request.method == 'POST':
+        loginusername= request.POST['loginusername']
+        loginpassword= request.POST['loginpassword']
+        user = authenticate(username = loginusername, password = loginpassword)
+
+        if user is not None:
+            login(request, user)
+            messages.success(request, "Successfully logged In")
+            return redirect('home')
+        
+        else:
+            messages.error(request, "Invalid Credentials please try again")
+            return redirect('home')
+
+    else:
+        return HttpResponse('Invalid request', status=404)
+
+def handleLogout(request):
+    if request.user.is_authenticated:
+        logout(request)
+        messages.success(request, "Successfully logged Out")
+        return redirect('home')
+    else:
+        return HttpResponse('Invalid request', status=404)
